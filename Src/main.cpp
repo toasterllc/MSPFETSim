b8ffdf68ff0c1cfe2f35255b9394e0ae06510fdc
Dave Keck <dave@heytoaster.com>
2021-07-21 02:54:17 -0700

    Cleanup

--- Src/main.cpp
+++ Src/main.cpp
@@ -4,28 +4,33 @@
 #include "Drivers/MSPInterfaceMDC.h"
 #include "Drivers/MSPInterfaceFTDI.h"
 
+std::unique_ptr<MSPInterface> CreateMSPInterface() {
+    // Dummy
+//    return std::make_unique<MSPInterfaceDummy>();
+    
+    // MDC
+//    auto devices = MSPInterfaceMDC::GetDevices();
+//    if (devices.empty()) throw RuntimeError("no matching USB devices");
+//    if (devices.size() > 1) throw RuntimeError("more than one matching USB device");
+//    return std::make_unique<MSPInterfaceMDC>(std::move(devices[0]));
+    
+    // FTDI
+    constexpr uint8_t TestPin = 0x01;
+    constexpr uint8_t RstPin = 0x02;
+    auto devices = MSPInterfaceFTDI::GetDevices();
+    if (devices.empty()) throw RuntimeError("no matching FTDI devices");
+    if (devices.size() > 1) throw RuntimeError("more than one matching FTDI device");
+    return std::make_unique<MSPInterfaceFTDI>(TestPin, RstPin, std::move(devices[0]));
+}
+
 int main(int argc, const char* argv[]) {
     try {
-//        MSPInterfaceDummy msp;
-        
-        auto devices = MSPInterfaceMDC::GetDevices();
-        if (devices.empty()) throw RuntimeError("no matching USB devices");
-        if (devices.size() > 1) throw RuntimeError("more than one matching USB device");
-        MSPInterfaceMDC msp(std::move(devices[0]));
-        
-//        constexpr uint8_t TestPin = 0x01;
-//        constexpr uint8_t RstPin = 0x02;
-//        auto devices = MSPInterfaceFTDI::GetDevices();
-//        if (devices.empty()) throw RuntimeError("no matching FTDI devices");
-//        if (devices.size() > 1) throw RuntimeError("more than one matching FTDI device");
-//        MSPInterfaceFTDI msp(TestPin, RstPin, std::move(devices[0]));
-        
-        MSPProbeSim probeSim(msp);
+        auto msp = CreateMSPInterface();
+        MSPProbeSim probeSim(*msp);
         probeSim.run();
     
     } catch (const std::exception& e) {
         fprintf(stderr, "Error: %s\n", e.what());
-        fprintf(stderr, "Exiting...\n");
         return 1;
     }
     
